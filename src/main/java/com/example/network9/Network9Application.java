765d005cd02f896a763fa58afc719aa05af95b83
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-06-13 10:21:50 +0200

    Initial commit

---
+++ src/main/java/com/example/network9/Network9Application.java
@@ -0,0 +1,13 @@
+package com.example.network9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Network9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Network9Application.class, args);
+	}
+
+}
